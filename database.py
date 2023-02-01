0b5ce61158b988b6f636101dd9fb7353b984275c
jagernau <stepanenkov25@gmail.com>
2023-02-01 02:52:31 +0200

    deploy5

--- database.py
+++ database.py
@@ -13,9 +13,12 @@ load_dotenv(dotenv_path)
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
+POSTGRES_HOST = os.environ.get("POSTGRES_HOST", default="localhost")
+POSTGRES_PORT = os.environ.get("POSTGRES_PORT", default=5432)
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DB}"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
