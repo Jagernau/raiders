f7ddd64a9fd08eac5e4dcbd73422b73bc5f5b8d5
Maxim <93077284+Jagernau@users.noreply.github.com>
2023-02-01 06:50:24 +0300

    new

--- crud.py
+++ crud.py
@@ -5,5 +5,5 @@ from models import FrameWorkModel
 def get_fram_from_lang(db: Session, language: str):
     return db.query(FrameWorkModel).filter(FrameWorkModel.language == language).first()
 
-def get_frams(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(FrameWorkModel).offset(skip).limit(limit).all()
+def get_frams(db: Session):
+    return db.query(FrameWorkModel).all()
